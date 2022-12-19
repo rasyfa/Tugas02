0d3445f89efd0bd1faa6e033316a4dde3a73cf23
rasyfa <rasyfa04@gmail.com>
2022-12-19 17:26:23 +0800

    Constructor dan Parameter

--- src/tugas02/RectangleAksi.java
+++ src/tugas02/RectangleAksi.java
@@ -9,5 +9,11 @@ public class RectangleAksi {
         rl.cetakInfo();
         System.out.println("Luas rectangle ="+rl.hitungLuas());
         rl.cetakLuas();
+        
+        Rectangle r2 = new Rectangle ();
+        r2.cetakInfo();
+        
+        Rectangle r3 = new Rectangle (50,30);
+        r3.cetakInfo();
     }
 }
