be9b8ae1b0682386d7bb0fb008ba501f0ca74052
rasyfa <rasyfa04@gmail.com>
2022-12-19 17:17:15 +0800

    Mengisi Nilai Atribute

--- src/tugas02/RectangleAksi.java
+++ src/tugas02/RectangleAksi.java
@@ -3,5 +3,7 @@ package tugas02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle rl = new Rectangle ();
+        rl.panjang = 9;
+        rl.lebar = 3;
     }
 }
